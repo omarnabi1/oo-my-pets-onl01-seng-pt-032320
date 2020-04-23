502f036abeb9633609abf238b9396fc3e8151c2c
Omar Nabi <onabi20@gmail.com>
2020-04-23 04:14:11 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,29 @@
 class Owner
-  # code goes here
+ attr_accessor :species, :name 
+ 
+ @@all = [ ]
+ 
+ def initialize (name, species = 'human')
+   @name = name
+   @species = species
+   @@all << self 
+ end
+ 
+ def say_species 
+   "I am a #{species}"
+ end
+ 
+ def self.all
+   @@all
+ end
+ 
+ def self.reset_all
+   @@all = Array.new 
+ end
+ 
+ def cats 
+  
+ 
+ 
+ 
 end
