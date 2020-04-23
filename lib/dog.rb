de0eb0bb72c09410d0e7b561e36df0ac70de4960
Omar Nabi <onabi20@gmail.com>
2020-04-23 04:19:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,16 @@
 class Dog
- attr_accessor :name, :mood, :owner
- @@all = [ ]
- 
- def initialize(name, owner)
-   @name = name 
-   @mood = "nervous"
-   @owener = owner 
-   @@all << self 
- end
- 
- def self.all 
-   @@all 
- end 
- 
+  attr_accessor :mood, :owner
+  attr_reader :name
+  @@all = Array.new
+  
+  def initialize(name,owner)
+    @name = name
+    @mood = "nervous"
+    @owner = owner
+    @@all.push(self)
+  end
+  
+  def self.all 
+    @@all
+  end
 end
